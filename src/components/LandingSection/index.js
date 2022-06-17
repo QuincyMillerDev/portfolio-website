683310c2ad148fb321f6d96db767b369a3307d6c
Quincy Miller <quincymiller6589@gmail.com>
2022-06-16 21:47:00 -0400

    first commit

---
+++ src/components/LandingSection/index.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react';
+import Video from '../../videos/video.mp4';
+import { Button } from '../ButtonElement';
+import { 
+    LandingContainer, 
+    LandingBg, 
+    VideoBg,
+    LandingContent,
+    LandingH1,
+    LandingP,
+    LandingBtnWrapper,
+    ArrowForward,
+    ArrowRight
+} from './LandingElements';
+
+const LandingSection = () => {
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    }
+
+  return (
+    <LandingContainer id='home'>
+        <LandingBg>
+            <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+        </LandingBg>
+        <LandingContent>
+            <LandingH1>Hi, I'm Quincy Miller</LandingH1>
+            <LandingP>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </LandingP>
+            <LandingBtnWrapper>
+                <Button to="skills" onMouseEnter={onHover} onMouseLeave={onHover}>
+                    Skills {hover ? <ArrowForward /> : <ArrowRight/>}
+                </Button>
+            </LandingBtnWrapper>
+        </LandingContent>
+    </LandingContainer>
+  )
+}
+
+export default LandingSection;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
+import { 
+  Nav, 
+  NavbarContainer, 
+  NavLogo, 
+  MobileIcon, 
+  NavMenu, 
+  NavItem, 
+  NavLinks,
+  NavBtn,
+  NavBtnLink
+} from './NavbarElements';
+
+
+const Navbar = ({ toggle }) => {
+  return (
+    <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to='/'>Quincy</NavLogo>
+                <MobileIcon onClick={toggle}>
+                  <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                  <NavItem>
+                    <NavLinks to="about">About</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to="discover">Discover</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to="services">Services</NavLinks>
+                  </NavItem>
+                  <NavItem>
+                    <NavLinks to="signup">Sign Up</NavLinks>
+                  </NavItem>
+                </NavMenu>
+            </NavbarContainer>
+        </Nav>
+    </>
+  );
+}
+
+export default Navbar
