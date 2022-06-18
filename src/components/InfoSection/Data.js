6d75c67048ead1c1bca93f0c37382af4c6e37d30
Quincy Miller <quincymiller6589@gmail.com>
2022-06-17 20:14:09 -0400

    first info section

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Lorem Ipsum',
+    headLine: 'filler text more filler',
+    description: 'subtitle filler text',
+    buttonLabel: 'hi I button',
+    imgStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,24 @@ import React, { useState } from 'react';
 import Sidebar from '../components/Sidebar';
 import Navbar from '../components/Navbar';
 import LandingSection from '../components/LandingSection/index';
+import InfoSection from '../components/InfoSection'
+import { homeObjOne } from '../components/InfoSection/Data';
 
 const Home = () => {
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false);
 
     const toggle = () => {
-        setIsOpen(!isOpen)
-    }
+        setIsOpen(!isOpen);
+    };
 
   return (
     <>
     <Sidebar isOpen={isOpen} toggle={toggle} />
     <Navbar toggle={toggle} />
     <LandingSection />
+    <InfoSection {...homeObjOne}/>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
